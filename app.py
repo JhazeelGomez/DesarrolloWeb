0c139de574ff91ee83432f2e3de51b8f8c878796
jhazz-Gomez <jhazzGomez89@outlook.com>
2024-09-27 20:48:25 -0600

    se agrego un nuevo boton para el grafico de dispersion

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+
 st.header('Histograma de Datos de vehiculos')
-        
+hist_button = st.button('Construir histograma') # crear un botón
+     
 if hist_button: # al hacer clic en el botón
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
@@ -16,6 +17,10 @@ if hist_button: # al hacer clic en el botón
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
+Scat_button = st.button('Construir grafico de dispersion')
+
+if Scat_button:
+    st.write('Creacion de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
 
     fig2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
     st.plotly_chart(fig2, use_container_width=True)
